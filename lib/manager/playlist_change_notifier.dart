f1fb108d32b0241e34bc5349c94836d69080dca0
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-05 22:23:01 +0530

    Create.Upload Manager

---
+++ lib/manager/playlist_change_notifier.dart
@@ -0,0 +1,10 @@
+import 'package:sahil/helper/imports/common_import.dart';
+
+import '../model/chat_message_model.dart';
+
+class PlaylistChangeNotifier extends ValueNotifier<List<ChatMessageModel>> {
+  PlaylistChangeNotifier() : super(_initialValue);
+  static final _initialValue = [
+    ChatMessageModel()
+  ];
+}
